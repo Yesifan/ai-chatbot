4c03187d6998989d173691c291fd49595fdd392b
思凡 叶 <yesifan66@qq.com>
2024-01-29 15:50:21 +0800

    [chore] support local db

--- config/server.ts
+++ config/server.ts
@@ -1,4 +1,5 @@
 export const config = {
+  POSTGRES_URL: process.env.POSTGRES_URL,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   OPENAI_BASEPATH: process.env.OPENAI_BASEPATH,
   OPENAI_TAG_ASSISTANT: process.env.OPENAI_TAG_ASSISTANT,
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,8 +1,1 @@
-import { createKysely } from '@vercel/postgres-kysely'
-import type { Database } from '../../types/database' // this is the Database interface we defined earlier
-
-// Database interface is passed to Kysely's constructor, and from now on, Kysely
-// knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
-// to communicate with your database.
-export default createKysely<Database>()
+export { default } from './db'
