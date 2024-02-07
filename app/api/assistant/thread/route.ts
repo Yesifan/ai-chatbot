29ac0150df4a927bca515613c8a28d52a34c999d
思凡 叶 <yesifan66@qq.com>
2024-02-07 17:22:22 +0800

    [chore] upstash use POST in cron job

--- app/api/assistant/thread/route.ts
+++ app/api/assistant/thread/route.ts
@@ -19,7 +19,7 @@ const getQueueThreads = () => {
     .execute()
 }
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const authHeader = request.headers.get('authorization')
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Unauthorized', {
